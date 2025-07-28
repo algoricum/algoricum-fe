9541a225b4c7f9e403820cae9b45ebba3d161a19
abdullah salman <abdullah.salman@hashlogics.com>
2025-07-28 11:57:42 +0500

    resolved ts error

--- src/components/common/Sidebar/index.tsx
+++ src/components/common/Sidebar/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-import type React from "react";
 import footerItems from "@/constants/footerItems";
 import { ErrorToast, SuccessToast } from "@/helpers/toast";
 import { X } from "lucide-react";
