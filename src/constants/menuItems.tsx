91d8b24c9dfc3de1d10e1b520bf42f21496b5515
Muhammad Ammaad Tehseen <97141225+MAmmaadTehseen@users.noreply.github.com>
2025-07-30 13:44:21 +0500

    Stripe Integration

--- src/constants/menuItems.tsx
+++ src/constants/menuItems.tsx
@@ -1,7 +1,8 @@
-import { ChatIcon,TicketIcon} from "@/icons";
+import { ChatIcon, TicketIcon } from "@/icons";
 import { DashboardIcon } from "@/icons";
 import { StaffManagementIcon } from "@/icons";
 // import { CalendarIcon } from "@/icons";
+import { BillingIcon } from "@/icons";
 
 const menuItems = [
   {
@@ -30,6 +31,13 @@ const menuItems = [
     icon: <StaffManagementIcon />,
     selectedicon: <StaffManagementIcon color="var(--color-primary-1000)" />,
     label: "Staff Management",
+    disabled: false,
+  },
+  {
+    key: "billing",
+    icon: <BillingIcon />,
+    selectedicon: <BillingIcon color="var(--color-primary-1000)" />,
+    label: "Billing",
     disabled: false,
   },
 ];
