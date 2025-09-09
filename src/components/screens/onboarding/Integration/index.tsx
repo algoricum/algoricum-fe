68d8b4b8fac2370975a28011e87537a93a9246b6
Muhammad Ammaad Tehseen <97141225+MAmmaadTehseen@users.noreply.github.com>
2025-09-09 18:33:37 +0500

    stripe cron

--- src/components/screens/onboarding/Integration/index.tsx
+++ src/components/screens/onboarding/Integration/index.tsx
@@ -178,7 +178,7 @@ export default function IntegrationsStep({ onNext, onPrev, initialData = {}, isS
         onNext(finalData);
       }
       setAutoProgressing(false);
-    }, 1500);
+    }, 2000);
   }, [
     autoProgressing,
     currentQuestionIndex,
